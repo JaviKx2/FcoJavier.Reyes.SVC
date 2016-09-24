ac9d153745b9cab2bdbaaaf8ef51e8aa4d1216f1
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2016-09-24 13:54:16 +0200

    Class C21Test created.

---
+++ src/test/java/es/upm/miw/fcojavier/reyes/svc/core/C21Test.java
@@ -0,0 +1,26 @@
+package es.upm.miw.fcojavier.reyes.svc.core;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C21Test {
+	C21 c21;
+
+	@Before
+	public void init() {
+		c21 = new C21();
+	}
+	
+	@Test
+	public void testM1() {	
+		assertEquals("m1", c21.m1());
+	}
+
+	@Test
+	public void testM2() {
+		assertEquals("m2", c21.m2());
+	}
+
+}
