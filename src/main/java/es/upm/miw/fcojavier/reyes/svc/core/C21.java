49d4769376c48d4fe911feabc83ef219cf56229f
JaviKx2 <javier.reyes.mangas@alumnos.upm.es>
2016-09-24 17:02:41 +0200

    Added method m3 into class C21.

--- src/main/java/es/upm/miw/fcojavier/reyes/svc/core/C21.java
+++ src/main/java/es/upm/miw/fcojavier/reyes/svc/core/C21.java
@@ -1,11 +1,15 @@
 package es.upm.miw.fcojavier.reyes.svc.core;
 
 public class C21 {
-	public String m1() {
-		return "m1";
-	}
+    public String m1() {
+        return "m1";
+    }
 
-	public String m2() {
-		return "m2";
-	}
+    public String m2() {
+        return "m2";
+    }
+
+    public String m3() {
+        return "m3";
+    }
 }
